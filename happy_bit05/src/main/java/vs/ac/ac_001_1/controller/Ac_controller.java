44618f394933ad82f9037a3d0b109cbd5baace8f
LeeHyuckHee <leehh4864@gmail.com>
2020-04-19 00:58:08 +0900

    마무리 커밋

--- happy_bit05/src/main/java/vs/ac/ac_001_1/controller/Ac_controller.java
+++ happy_bit05/src/main/java/vs/ac/ac_001_1/controller/Ac_controller.java
@@ -1,17 +1,45 @@
 package vs.ac.ac_001_1.controller;
 
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import org.springframework.web.servlet.ModelAndView;
+import vs.ac.ac_001_1.service.Ac_Service;
+import vs.ac.ac_001_1.vo.AcVO;
 
-public interface Ac_controller {
- 
-	public ModelAndView Ac_List(HttpServletRequest request, HttpServletResponse response) throws Exception;
-
+@Controller
+@RequestMapping("/ac/*")
+public class Ac_Controller {
 	
+	@Autowired
+	private Ac_Service ac_service;
+
+	@GetMapping(value="/ac_002_1")
+	public void ac_List(Model model ,AcVO acvo) throws Exception {
+		
+		
+		 model.addAttribute("data", ac_service.ac_List(acvo)); 
+		
+	}
 	
-	
+	@PostMapping("/ac_001_1.do")
+	public String ac_register(AcVO acvo, RedirectAttributes rttr) {
+		
+		System.out.println("register:" + acvo);
+		
+		ac_service.ac_insert(acvo);
+		
+		rttr.addFlashAttribute("result", acvo.getCo_b_index());
+		
+		return "redirect:/ac/ac_002_1";
+	}
+	@GetMapping("/ac_001_1")
+	public void ac_register() {
+		
+	}
 }
