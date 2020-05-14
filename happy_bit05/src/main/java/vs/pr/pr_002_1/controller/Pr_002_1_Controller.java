47732ed8773540340ab9dd154c374fc420432562
이찬영 <enrjsj123@naver.com>
2020-05-14 18:17:32 +0900

    dkdkdkdkdkdk

--- happy_bit05/src/main/java/vs/pr/pr_002_1/controller/Pr_002_1_Controller.java
+++ happy_bit05/src/main/java/vs/pr/pr_002_1/controller/Pr_002_1_Controller.java
@@ -1,6 +1,7 @@
 package vs.pr.pr_002_1.controller;
 
 
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import vs.ac.ac_001_1.vo.AcVO;
 import vs.co.co_001_1.dto.Page_DTO;
 import vs.co.co_001_1.vo.PageUtil;
 import vs.pr.pr_002_1.service.Pr_002_1_Service;
@@ -27,7 +29,30 @@ public class Pr_002_1_Controller {
 	//개인기록 리스트
 	@GetMapping("/pr_002_1")
 	public void pr_list(Model model,Page_DTO dto) {
-		
+		/*
+		List<Pr_002_1VO> prvo = pr_service.pr_list(dto);
+		//리스트 썸네일 인코딩, 디코딩 작업.
+        for(int i = 0; i < prvo.size(); i++) {
+           
+        	Pr_002_1VO vo = prvo.get(i);
+            
+           if(vo.getCo_thumbnail() != null) {
+              
+              byte[] imageContent = Base64.getEncoder().encode(vo.getCo_thumbnail());
+              
+              //System.out.println("대체 뭐야........" + imageContent);
+              String thumbnail = new String(imageContent);
+              
+              System.out.println(vo.getCo_request() + "ㅇㅇㅇㅇㅇ");	
+              //System.out.println(thumbnail);
+           
+              vo.setCo_request(thumbnail);
+           // model.addAttribute("thumbnail"+i, thumbnail);
+           }else {
+              vo.setCo_request("");
+           }
+        }
+		*/
 		System.out.println("개인기록 리스트 ????");
 		
 		List<Pr_002_1VO> prvo = pr_service.pr_list(dto);
